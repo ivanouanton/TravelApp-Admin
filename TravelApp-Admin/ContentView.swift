4839e04afc857c603e429be5750b1533f8b3f1f4
Aliaksei Petrusevich <water.waters@yandex.by>
2020-04-16 21:18:15 +0300

    Initial Commit

---
+++ TravelApp-Admin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravelApp-Admin
+//
+//  Created by Антон Иванов on 4/16/20.
+//  Copyright © 2020 companyName. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
