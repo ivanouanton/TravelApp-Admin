2ec07c2c3482837aa354b8b49c38fe6ba0899029
Anton Ivanov <anivanov@scand.com>
2020-04-17 18:02:46 +0300

    Update pods

--- Podfile
+++ Podfile
@@ -1,13 +1,11 @@
-# Uncomment the next line to define a global platform for your project
 platform :ios, '9.0'
 
 target 'TravelApp-Admin' do
-  # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
 	pod 'Moya'
   pod 'CoreXLSX'
+  pod 'SwiftyDropbox'
 
-  # Pods for TravelApp-Admin
 
 end
--- TravelApp-Admin/AppDelegate.swift
+++ TravelApp-Admin/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyDropbox
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        DropboxClientsManager.setupWithAppKey("g6bhcu31gt08crd")
         return true
     }
 
